67db0e9e0970d6fe143e92b7106f0530a8afe2ad
AashayBharadwaj <64611037+AashayBharadwaj@users.noreply.github.com>
2021-05-09 22:44:22 +0530

    some beautification

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var readlineSync = require('readline-sync');
 var recipe_names=["dosa_batter","idli_batter","adai_batter","pessarattu_batter"]
 console.log(recipe_names)
 
-var input_batter = readlineSync.question("Choose an item from the above list?")
+var input_batter = readlineSync.question("Choose an item from the above list?\n")
 
 
-var input_quantity = readlineSync.question("Choose the quantity of the batter")
+var input_quantity = readlineSync.question("Choose the quantity of the batter\n")
 
 
 var items = [
@@ -23,7 +23,7 @@ var items = [
           },
     
     { 
-      batter_name: "idli_batter ",
+      batter_name: "idli_batter",
       idliRice:400,
       uradDal:250,
       nylonSabudana:400,
@@ -59,15 +59,18 @@ var items = [
           // key_1=items[i].uradDal
           // updated_quantity=key_1*3
           var ingredients=Object.keys(items[i])
+          // hold array of keys
           console.log(ingredients)
 
           var quantities=Object.values(items[i])
+          // hold array of values
+
           // console.log(quantities)
 
           for (j=0;j<quantities.length;j++)
           {
             var new_quantity = quantities[j]*input_quantity
-            console.log(new_quantity)
+            console.log( new_quantity + "grams\n")
           }
           
 
@@ -75,12 +78,34 @@ var items = [
           // console.log(items[i].poha)
 
           }
-
+   
        
 
     }
 
 
+
+
+
+
+
+// let a = {x: 200, y: 1, z:0,k:4}
+// let attributes = Object.keys(a)
+// console.log(attributes)
+// console.log(a.x)
+
+
+// for (i=0;i<attributes.length;i++)
+// {
+//   current_value=(attributes[i])
+//   console.log(current_value)
+//   console.log(a.current_value) 
+  
+// }
+
+
+    
+    
 
 
 
